95846417149ecfe53e3101d865306c8c37cd6539
davidjack7 <davidjackmetcalf@gmail.com>
2016-01-12 19:02:04 +0100

    User profile OK!

--- application/views/profile.php
+++ application/views/profile.php
@@ -29,13 +29,14 @@
 }
 </style>
 
-<div class="container" style="width: 70%; margin: 0 auto;">
+<div class="container" style="width: 70%; margin: 0 auto; color: black;">
     <div class="fb-profile">
-        <img align="left" class="fb-image-lg" src="http://lorempixel.com/850/280/nightlife/5/" alt="Profile image example"/>
-        <img align="left" class="fb-image-profile thumbnail" src="http://lorempixel.com/180/180/people/9/" alt="Profile image example"/>
+        <img align="left" class="fb-image-lg" src="http://lorempixel.com/850/280/" alt="Profile image example"/>
+        <img align="left" class="fb-image-profile thumbnail" src="http://lorempixel.com/180/180/people/" alt="Profile image example"/>
         <div class="fb-profile-text">
-            <h1>Eli Macy</h1>
-            <p>Girls just wanna go fun.</p>
+            <h1><?php echo $user['nick']; ?></h1>
+            <h2><?php echo $user['name'] . ' ' .$user['surname'];?></h2>
+            <h3><?php echo $user['email'];?></h3>
         </div>
     </div>
 </div>
