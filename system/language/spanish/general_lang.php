94894202c3e08307ce4bf6f18abff2b38698d4ec
davidjack7 <davidjackmetcalf@gmail.com>
2016-01-13 01:14:28 +0100

    Added translation files for EN & ES.

---
+++ system/language/spanish/general_lang.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Diego
+ * Date: 12/01/16
+ * Time: 17:09
+ */
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014 - 2015, British Columbia Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package	CodeIgniter
+ * @author	EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (http://ellislab.com/)
+ * @copyright	Copyright (c) 2014 - 2015, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	http://codeigniter.com
+ * @since	Version 1.0.0
+ * @filesource
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*DASHBOARD*/
+$lang['titulo_dashboard'] = 'Dashboard';
+$lang['nav_activity'] = 'Actividad';
+$lang['nav_actions'] = 'Acciones';
+
+$lang['header_profile'] = 'Perfil';
+$lang['header_logout'] = 'Salir';
+
+
+$lang['blogs_edit'] = 'Editar';
+$lang['blogs_delete'] = 'Eliminar';
+
+$lang['activity_title'] = 'Título';
+$lang['activity_date'] = 'Fecha';
+
+$lang['actions_title'] = 'Acciones';
+$lang['actions_button'] = 'Abrir blog';
+$lang['actions_model_title'] = 'Crea tu nuevo Dominio & Blog';
+$lang['actions_model__blog_title'] = 'Título';
+$lang['actions_model__blog_descr'] = 'Descripción';
+$lang['actions_model__blog_save'] = 'Guardar';
+$lang['actions_model__blog_close'] = 'Cerrar';
+
+
+
+/*BLOGS*/
+$lang['description'] = 'Descripción';
+$lang['new_post'] = 'Añadir un post';
+$lang['post_author'] = 'Autor';
+
+$lang['post_edit'] = 'Editar';
+$lang['post_delete'] = 'Eliminar';
+
+$lang['blog_model_title'] = 'Añade un nuevo post';
+$lang['blog_model_post_title'] = 'Título';
+$lang['blog_model_post_content'] = 'Contenido';
+$lang['blog_model_post_save'] = 'Guardar';
+$lang['blog_model_post_close'] = 'Cerrar';
+
+
